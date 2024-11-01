939ee68f2003a2482282463e87d6b68b20921766
Josh Errickson <josh@errickson.net>
2024-11-01 15:28:44 -0400

    Initial Commit

---
+++ MacDeck/MacDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacDeckApp.swift
+//  MacDeck
+//
+//  Created by Josh on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MacDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacDeckTests/MacDeckTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacDeckTests.swift
+//  MacDeckTests
+//
+//  Created by Josh on 11/1/24.
+//
+
+import Testing
+@testable import MacDeck
+
+struct MacDeckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
